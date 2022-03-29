40beeb28005a802134104aac8f78eccf0e44b1d5
Quy Thanh <quythanh2205@gmail.com>
2022-03-29 20:43:05 +0700

    fix bug

--- assets/js/info.js
+++ assets/js/info.js
@@ -1,33 +1,34 @@
 var curID = localStorage.getItem('curID') || 0
-var f = listFilm[curID]
-document.querySelector('.info').innerHTML = `
-<div class="info__banner">
-    <img src="${f.image}" alt="">
-</div>
-
-<div class="info__img">
-    <img src="${f.image}" alt="">
-</div>
-
-<div class="info__btn-play">
-    <i class="fas fa-play"></i>
-    Xem ngay
-</div>
-
-<div class="info__details">
-    <div class="info__name">${f.name}</div>
-    <div class="info__view">View: ${new Intl.NumberFormat().format(f.view)}</div>
-    <div class="info__category">Category: ${f.category}</div>
-    <div class="info__desc">${f.desc}</div>
-</div>
-
-<div class="video">
-    <div class="video__inner">
-        <iframe width="1120" height="630" src="${f.link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+listFilm.then(res => {
+    let f = res[curID]
+    document.querySelector('.info').innerHTML = `
+    <div class="info__banner">
+        <img src="${f.image}" alt="">
     </div>
-</div>
-`
 
+    <div class="info__img">
+        <img src="${f.image}" alt="">
+    </div>
+
+    <div class="info__btn-play">
+        <i class="fas fa-play"></i>
+        Xem ngay
+    </div>
+
+    <div class="info__details">
+        <div class="info__name">${f.name}</div>
+        <div class="info__view">View: ${new Intl.NumberFormat().format(f.view)}</div>
+        <div class="info__category">Category: ${f.category}</div>
+        <div class="info__desc">${f.desc}</div>
+    </div>
+
+    <div class="video">
+        <div class="video__inner">
+            <iframe width="1120" height="630" src="${f.link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+    `
+})
 const video = document.querySelector('.video')
 document.querySelector('.info__btn-play').addEventListener('click', () => toggle('.video', 'show'))
 
